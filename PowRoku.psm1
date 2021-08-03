61b1c068299a4cedeaa085e9731ba7ffd971ec54
James Brundage <james.brundage@start-automating.com>
2021-08-03 02:21:52 -0700

    PowRoku.psm1: Ensuring System.Web is always loaded

--- PowRoku.psm1
+++ PowRoku.psm1
@@ -1,4 +1,7 @@
-﻿foreach ($file in Get-ChildItem -Path $PSScriptRoot -Filter *-*.ps1) {
+﻿if (-not ('Web.HttpUtility' -as [Type])) {
+    Add-Type -AssemblyName System.Web
+}
+foreach ($file in Get-ChildItem -Path $PSScriptRoot -Filter *-*.ps1) {
     if ($file.Name -notlike '*.*.ps1') {
         . $file.fullname
     }

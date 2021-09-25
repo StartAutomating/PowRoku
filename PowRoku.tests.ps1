06814820aa1d8875c5dfffc1efe4ffad6a7ec30b
James Brundage <james.brundage@start-automating.com>
2021-09-25 13:20:50 -0700

    Updating PowRoku.tests.ps1 (using a dummy ip address, not loopback)

--- PowRoku.tests.ps1
+++ PowRoku.tests.ps1
@@ -14,25 +14,30 @@
 
 describe "PowRoku" {
     it 'Can send keys to a Roku' {
-        $whatIf = Send-Roku -IPAddress ([IPAddress]::Loopback) -KeyPress VolumeUp -WhatIf
+        $whatIf = Send-Roku -IPAddress 1.2.3.4 -KeyPress VolumeUp -WhatIf
         $whatIf.Uri |Should -BeLike */keypress/VolumeUp
         $whatIf.Method |Should -Be POST
     }
 
     it 'Can send a command to a Roku' {
-        $whatIf = Send-Roku -IPAddress ([IPAddress]::Loopback) -Command query/device-info -WhatIf
+        $whatIf = Send-Roku -IPAddress 1.2.3.4 -Command query/device-info -WhatIf
         $whatIf.Uri |Should -BeLike */query/device-info
         $whatIf.Method |Should -Be GET
     }
 
     it 'Can send text to a Roku' {
-        $whatIf = Send-Roku -IPAddress ([IPAddress]::Loopback) -Text 'Rick & Morty' -WhatIf
+        $whatIf = Send-Roku -IPAddress 1.2.3.4 -Text 'Rick & Morty' -WhatIf
         $whatIf[0].Uri |Should -BeLike */keypress/Lit_R
         $whatIf[5].Uri |Should -BeLike */keypress/Lit_%26
     }
 
     it 'Can change the channel' {
-        $whatIf = Send-Roku -IPAddress ([IPAddress]::Loopback) -Channel 42
+        $whatIf = Send-Roku -IPAddress 1.2.3.4 -Channel 42
         $whatIf.URI | Should -BeLike */launch/tvinput.dtv?ch=42
+    }
+
+    it 'Can switch to HDMI' {
+        $whatIf = Send-Roku -IPAddress 1.2.3.4 -HDMIInput 1
+        $whatIf.URI | Should -BeLike */launch/tvinput.hdmi1
     }
 }
